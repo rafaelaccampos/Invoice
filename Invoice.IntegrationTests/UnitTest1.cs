35e3217ce9dc6d0db02d7606c61f4ddf5393aa26
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-09-07 20:05:36 -0300

    Inital project

---
+++ Invoice.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Invoice.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
