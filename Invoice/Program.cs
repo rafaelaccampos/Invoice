d078be9fc501e9165a3604b82af5c1871bc66857
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-09-07 22:03:45 -0300

    config: Add fluent migrator configuration

--- Invoice/Program.cs
+++ Invoice/Program.cs
@@ -1,35 +1,21 @@
-using System.Text.Json.Serialization;
+using FluentMigrator.Runner;
+using Invoice.Data.Migrations;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
-
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+builder.Services.AddFluentMigratorCore()
+    .ConfigureRunner(rb =>
+        rb.AddSqlServer()
+        .WithGlobalConnectionString(builder.Configuration.GetConnectionString("Invoice"))
+        .ScanIn(typeof(AddTables).Assembly).For.Migrations()
+    );
+
+var scope = app.Services.CreateScope();
+{
+    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
 };
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
-
 app.Run();
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-
-}
