5d0e9c7e6bdc7aa3d9789e7b01688e9e9bfbf606
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-09-07 21:36:34 -0300

    config: Add tables in migration

---
+++ Invoice.Data/Migrations/1_Add_Tables.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace Invoice.Data.Migrations
+{
+    [Migration(1)]
+    public class AddTables : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("contracts")
+                .WithColumn("id_contract").AsGuid().PrimaryKey()
+                .WithColumn("description").AsString()
+                .WithColumn("amount").AsDecimal()
+                .WithColumn("periods").AsInt32()
+                .WithColumn("date").AsDateTime();
+            Create.Table("payments")
+                .WithColumn("id_payment").AsGuid().PrimaryKey()
+                .WithColumn("id_contract").AsGuid().ForeignKey("contracts", "id_contract")
+                .WithColumn("amount").AsInt32()
+                .WithColumn("date").AsDateTime();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("payments");
+            Delete.Table("contracts");
+        }
+    }
+}
