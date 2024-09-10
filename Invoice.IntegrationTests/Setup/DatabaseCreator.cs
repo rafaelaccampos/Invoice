967a335920a46e65ec99fc9d27814d2257b3ce9e
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-09-09 21:30:56 -0300

    config: Add configuration for integration test

---
+++ Invoice.IntegrationTests/Setup/DatabaseCreator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Data.SqlClient;
+
+namespace Invoice.IntegrationTests.Setup
+{
+    public static class DatabaseCreator
+    {
+        public static void CreateDatabase(string connectionString)
+        {
+            var connectionBuilder = new SqlConnectionStringBuilder(connectionString);
+        }
+    }
+}
