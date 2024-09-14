c1bad241b29a6cd9f1609b39a52dee3410b2dbbb
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-09-14 18:51:18 -0300

    config: Add tests integration configuration

--- Invoice.IntegrationTests/Setup/DatabaseCreator.cs
+++ Invoice.IntegrationTests/Setup/DatabaseCreator.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 
 namespace Invoice.IntegrationTests.Setup
 {
@@ -8,6 +7,17 @@ namespace Invoice.IntegrationTests.Setup
         public static void CreateDatabase(string connectionString)
         {
             var connectionBuilder = new SqlConnectionStringBuilder(connectionString);
+            var databaseName = connectionBuilder.InitialCatalog;
+
+            var connectionStringMaster = new SqlConnectionStringBuilder(connectionBuilder.ConnectionString) { InitialCatalog = "master" }.ConnectionString;
+            
+            using var connection =  new SqlConnection(connectionStringMaster);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = $@"IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'{databaseName}')
+                              CREATE DATABASE [{databaseName}]";
+            command.ExecuteScalar();
         }
     }
 }
