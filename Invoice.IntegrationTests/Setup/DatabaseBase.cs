ecbe9cc6db1c885cec16cd2335d8a0231ffc6020
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-09-15 22:18:45 -0300

    config: Add tests integration configuration

---
+++ Invoice.IntegrationTests/Setup/DatabaseBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Invoice.IntegrationTests.Setup
+{
+    public class DatabaseBase
+    {
+        protected static IServiceScope _scope;
+        protected static InvoiceContext _context;
+        protected Faker Faker { get; } = new Faker("pt_BR");
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        public static T GetService<T>
+    }
+}
