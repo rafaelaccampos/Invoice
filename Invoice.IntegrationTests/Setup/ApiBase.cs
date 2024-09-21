9ab35f6a3daea735094110f53e594b21ba8248e9
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-09-20 22:11:55 -0300

    config: Add tests configuration

---
+++ Invoice.IntegrationTests/Setup/ApiBase.cs
@@ -0,0 +1,18 @@
+namespace Invoice.IntegrationTests.Setup;
+
+public class ApiBase : DatabaseBase 
+{
+    protected HttpClient _httpClient;
+
+    [SetUp]
+    public new void Setup()
+    {
+        _httpClient = TestEnvironment.Factory.CreateClient();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        _httpClient.Dispose();
+    }
+}
