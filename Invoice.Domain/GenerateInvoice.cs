4cb94b659f17196fb80f95d63c10e9b7365ff69d
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2025-01-18 18:17:20 -0300

    ref: Refactor code

--- Invoice.Domain/GenerateInvoice.cs
+++ Invoice.Domain/GenerateInvoice.cs
@@ -13,11 +13,11 @@ public class GenerateInvoice(IContractRepository contractRepository)
         {
             var invoices = contract.GenerateInvoices(contractInput.Type, contractInput.Month, contractInput.Year);
 
-            invoices.Select(invoice => new InvoiceOutput
+            invoicesOutput = invoices.Select(invoice => new InvoiceOutput
             {
                 Date = invoice.Date,
                 Amount = invoice.Amount
-            }).ToList().ForEach(invoiceOutput => invoicesOutput.Add(invoiceOutput));
+            }).ToList();
         }
 
         return invoicesOutput;
