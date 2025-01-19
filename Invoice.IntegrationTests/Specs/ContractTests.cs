5047b13754b9f5a0ea6dc0cc6682da0ffeb9c67b
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2025-01-19 17:47:57 -0300

    ref: Refactor code

---
+++ Invoice.IntegrationTests/Specs/ContractTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Invoice.Domain.Contracts.Entities;
+
+namespace Invoice.IntegrationTests.Specs;
+
+public class ContractTests
+{
+    [Test]
+    public void DeveGerarFaturasDeUmContrato()
+    {
+        var contract = new Contract("Mensalidade faculdade", 6000, 12, new DateTime(2024, 01, 10));
+
+        var invoices = contract.GenerateInvoices("accrual", 1, 2024);
+
+        using (new AssertionScope())
+        {
+            invoices.First().Date.Should().Be(new DateTime(2024, 01, 10));
+            invoices.First().Amount.Should().Be(500);
+        }
+    }
+}
